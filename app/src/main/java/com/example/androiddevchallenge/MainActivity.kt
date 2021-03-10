ac4ac9fc383f0c548aa81b13544fe2bea78069e4
Ivan Aguayo <leo.ivan.aguayo@gmail.com>
2021-03-09 20:08:41 -0800

    added all the requirements

--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -19,12 +19,24 @@ import android.os.Bundle
 import android.text.format.DateUtils
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.animation.*
+import androidx.compose.animation.Crossfade
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.animation.core.animateValueAsState
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
-import androidx.compose.runtime.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.material.LinearProgressIndicator
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.ProgressIndicatorDefaults
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
@@ -89,7 +101,8 @@ fun AnimatedClock(startTime: Long, timeRemaining: Long) {
     Column(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(top = 200.dp), horizontalAlignment = Alignment.CenterHorizontally
+            .padding(top = 200.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
 
         Crossfade(targetState = timeRemaining) {
